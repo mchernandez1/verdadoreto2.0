72525e7a3ca1bb539a84148c3b5543072e497e50
mchernandez1 <mc.hernandez1@uniandes.edu.co>
2018-10-08 21:02:34 -0500

    Guardar el registro de usuario aquí!

--- server/main.js
+++ server/main.js
@@ -4,4 +4,4 @@ import '../imports/api/salas.js';
 Meteor.startup(()=>{
     
 });
-
+//Considero que para la próxima versión de la aplicación podrían trabajar un en hacer parte de la lógica en el servidor. Es decir, manejar el registro de usuarios podría ser algo que podrían manejar desde está parte.
